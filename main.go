cf7aef5b09fb35450d11ca6053d71b6bc12f7f6c
m.gladyshev <secret.anry@gmail.com>
2025-07-18 15:21:55 +0300

    clean

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
-
 func main() {
-	toSort := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
-	rand.Shuffle(len(toSort), func(i, j int) {
-		toSort[i], toSort[j] = toSort[j], toSort[i]
-	})
-	res := QuickSort(toSort)
-	for i := 0; i < len(res); i++ {
-		fmt.Println(res[i])
-	}
 }
