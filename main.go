53d6c03d8fc631d4c3ad84f7506029ffc1a17c1f
m.gladyshev <secret.anry@gmail.com>
2025-07-18 16:20:05 +0300

    clean

--- main.go
+++ main.go
@@ -1,21 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"math/big"
-)
-
 func main() {
-	a := "12345678901234567890"
-	b := "98765432109876543210"
-
-	x := new(big.Int)
-	y := new(big.Int)
-	x.SetString(a, 10)
-	y.SetString(b, 10)
-
-	fmt.Println("BigAddInt:", BigAddInt(x, y))
-	fmt.Println("BigSubInt:", BigSubInt(y, x))
-	fmt.Println("BigMulInt:", BigMulInt(x, y))
-	fmt.Println("BigDivInt:", BigDivInt(y, x))
 }
