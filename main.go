42460bbad23add1d8b0943f8a9301feddc71c87b
m.gladyshev <secret.anry@gmail.com>
2025-07-18 16:47:00 +0300

    clean

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	cases := []string{
-		"abcd",
-		"abCdefAaf",
-		"aabcd",
-		"abcABC",
-		"😀😃😄😁",
-		"😀😃😄😀",
-	}
-	for _, s := range cases {
-		fmt.Printf("%q: %v\n", s, UniqueChars(s))
-	}
 }
