0a55f1bc8d8c1f9029d19f0a8ac94a0efd97136e
m.gladyshev <secret.anry@gmail.com>
2025-07-18 16:28:24 +0300

    clean

--- main.go
+++ main.go
@@ -1,15 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	ints := []int{10, 20, 30, 40, 50}
-	fmt.Println("Before RemoveAt:", ints)
-	ints = RemoveAt(ints, 2)
-	fmt.Println("After RemoveAt (index 2):", ints)
-
-	words := []string{"cat", "dog", "fish", "tree"}
-	fmt.Println("Before RemoveAt:", words)
-	words = RemoveAt(words, 1)
-	fmt.Println("After RemoveAt (index 1):", words)
 }
